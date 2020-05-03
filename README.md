f5be68e4f61eb170635e8f284ec5f09e0c6169b1
Chris Lahaye <dev@chrislahaye.com>
2020-05-03 14:40:46 +0200

    Add async handlers, request data, options, and remove sequential stuff
    Signed-off-by: Chris Lahaye <dev@chrislahaye.com>

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PM2 messages
-This library provides a communication interface to retrieve messages of sibling instances in a PM2 cluster. It can be used to aggregate Prometheus metrics such that the aggregated registry represents the cluster metrics.
+This package provides a communication interface to retrieve messages of processes managed by pm2. It can be used to aggregate Prometheus metrics such that the aggregated registry represents cluster metrics.
 
-### Installation
+## Installation
 Install the package with:
 
 ```sh
@@ -14,24 +14,49 @@ or
 yarn add pm2-messages
 ```
 
-### Usage
-Attach the message handler:
+## Quick start
+1. Attach a message handler
 
 ```js
 const pm2mes = require('pm2-messages');
 const prom = require('prom-client');
 
-pm2mes.onMessage('get_prom_register', (packet) => prom.register.getMetricsAsJSON());
+pm2mes.onMessage('get_prom_register', () => prom.register.getMetricsAsJSON());
 ```
 
-Collect all messages:
+2. Collect all messages
 
 ```js
 const metricsArr = await pm2mes.getMessages('get_prom_register');
 ```
 
-Aggregate the metrics:
+3. Aggregate the metrics
 
 ```js
 const metrics = prom.AggregatorRegistry.aggregate(metricsArr).metrics();
 ```
+
+## API
+### Message handlers
+Message handlers return the requested data for a specific topic. Handlers may be asynchronous, i.e. return a promise. Requests may carry additional data, available as first argument.
+
+```js
+pm2mes.onMessage('get_something', async (data) => {
+  if (data.val === myVal) return doSomethingAsync();
+});
+```
+
+### Collecting messages
+Messages can be collected using the `getMessages` function. The first argument is required and denotes the topic, the second argument is optional and contains additional request data, and the third argument is optional and contains configuration options.
+
+```js
+const somethingArr = await pm2mes.getMessages(
+  'get_something',
+  { val: 'value' },
+  { filter: (process) => process.name === 'my process', timeout: 100 }
+);
+```
+
+### Configuration
+- *filter*: Filter function to select the processes managed by pm2 from which messages need to be requested. Defaults to processes with same name as the active process, i.e. `(process): boolean => process.name === process.env.name`.
+- *timeout*: Timeout in milliseconds (ms). Default to 1000 ms.
--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,153 @@
 import * as assert from 'assert';
 import * as pm2 from 'pm2';
 
-interface Packet { topic: string; data: any }
-
-const handlers: { [key: string]: (packet: Packet) => any } = {};
-const instanceId = Number(process.env.pm_id);
-const instanceName = process.env.name;
+/**
+ * Handles a message request by returning the requested message.
+ */
+export type MessageHandler = (data?: any) => any;
 
 /**
- * Message listener
+ * Packet used to request messages. Sent to processes managed by pm2, using the pm2 bus.
  */
-process.on('message', async (packet: Packet): Promise<void> => {
-  if (typeof handlers[packet.topic] === 'function' && process.send) {
-    const data = await handlers[packet.topic](packet);
+interface RequestPacket { topic: string; data: { targetInstanceId: number; data?: any } };
 
-    process.send({ type: `process:${packet.data.targetInstanceId}`, data });
+/**
+ * Packet used to reply the requested message. Sent to the requesting process managed by pm2, using the Node.js IPC channel.
+ */
+interface ResponsePacket<T> { type: string; data: T };
+
+/**
+ * Attached message handles.
+ */
+const handlers: { [key: string]: MessageHandler } = {};
+
+/**
+ * The pid of the current process for the pm2 God daemon process.
+ */
+const myPmId = Number(process.env.pm_id);
+
+/**
+ * The name of the current process given in the original start command.
+ */
+const myName = process.env.name;
+
+/**
+ * Handles a request packet.
+ */
+process.on('message', async ({ topic, data: { targetInstanceId, data } }: RequestPacket): Promise<void> => {
+  if (typeof handlers[topic] === 'function' && process.send) {
+    const response: ResponsePacket<any> = {
+      type: `process:${targetInstanceId}`,
+      data: await handlers[topic](data),
+    };
+
+    process.send(response);
   }
 });
 
 /**
- * Retrieves messages from sibling instances.
+ * Options for the `getMessages` function.
  */
-export const getMessages = async function getMessages(topic: string, timeout: number = 1000): Promise<any[]> {
-  return new Promise(async (resolve: (messages: any[]) => void, reject): Promise<void> => {
-    assert.equal(typeof handlers[topic], 'function');
+export interface GetMessagesOptions {
+  /**
+   * Filter function to select the processes managed by pm2 from which messages need to be requested.
+   * Defaults to processes with same name as the active process.
+   */
+  filter?: (process: pm2.ProcessDescription) => boolean;
 
-    const packet = { topic, data: { targetInstanceId: instanceId } };
-    const request = {
-      pending: 0,
-      responses: [await handlers[topic](packet)],
-      timeout: setTimeout((): void => reject(new Error(`${topic} timed out`)), timeout),
-    };
+  /**
+   * Timeout in milliseconds (ms).
+   * Defaults to 1000 ms.
+   */
+  timeout?: number;
+}
 
-    const done = function done(err?: Error): void {
-      clearTimeout(request.timeout);
+/**
+ * Requests messages from processes managed by pm2.
+ */
+export const getMessages = function getMessages<T = any>(
+  topic: string,
+  data?: any,
+  {
+    filter = (process): boolean => process.name === myName,
+    timeout = 1000,
+  }: GetMessagesOptions = {}
+): Promise<T[]> {
+  assert.equal(typeof handlers[topic], 'function', `Handler for ${topic} not attached or not a function`);
+
+  return new Promise<T[]>((resolve, reject): void => {
+    const timer = setTimeout((): void => reject(new Error(`${topic} timed out`)), timeout);
+    const done = function done(err: Error | null, messages: T[]): void {
+      clearTimeout(timer);
 
       if (err) reject(err);
-      else resolve(request.responses);
+      else resolve(messages);
     };
 
     pm2.connect(false, (err): void => {
-      if (err) return done(err);
+      if (err) return done(err, []);
 
-      pm2.list((err, processes): void => {
-        if (err) return done(err);
+      new Promise<T[]>((resolve, reject): void => {
+        pm2.list((err, processes): void => {
+          if (err) return reject(err);
 
-        const instances = processes.filter((process: any): boolean =>
-          process.name === instanceName && process.pm_id !== instanceId);
+          const targets: number[] = [];
+          const messages: T[] = [];
+          const promises: Promise<void>[] = [];
 
-        if (instances.length) request.pending += instances.length;
-        else return done();
+          for (const process of processes) {
+            if (!filter(process)) continue;
 
-        const listener = new Promise((resolve, reject): void => {
-          pm2.launchBus((err, bus): void => {
-            if (err) return reject(err);
-
-            bus.on(`process:${instanceId}`, (packet: any): void => {
-              request.responses.push(packet.data);
-              request.pending -= 1;
-
-              if (!request.pending) resolve();
-            });
-          });
-        });
-
-        instances.forEach((instance): void => {
-          if (typeof instance.pm_id === 'number') {
-            pm2.sendDataToProcessId(instance.pm_id, packet, (err: Error): void => err && done(err));
+            if (process.pm_id === myPmId) {
+              promises.push(
+                Promise.resolve()
+                  .then((): T | Promise<T> => handlers[topic](data))
+                  .then((message): void => { messages.push(message) })
+              );
+            } else if (typeof process.pm_id === 'number') {
+              targets.push(process.pm_id);
+            }
           }
-        });
 
-        listener.then((): void => done(), done);
-      });
+          if (targets.length) {
+            promises.push(new Promise((resolve, reject): void => {
+              pm2.launchBus((err, bus): void => {
+                if (err) return reject(err);
+
+                let pending = targets.length;
+
+                bus.on(`process:${myPmId}`, ({ data }: ResponsePacket<T>): void => {
+                  messages.push(data);
+                  pending -= 1;
+
+                  if (!pending) resolve();
+                });
+
+                const request: RequestPacket = { topic, data: { targetInstanceId: myPmId, data } };
+
+                targets.forEach((pmId): void => {
+                  pm2.sendDataToProcessId(pmId, request, (err: Error): void => err && reject(err));
+                });
+              });
+            }));
+          }
+
+          Promise.all(promises)
+            .then((): void => resolve(messages))
+            .catch(reject);
+        });
+      })
+        .then((messages): void => done(null, messages))
+        .catch((err): void => done(err, []))
+        .finally((): void => pm2.disconnect());
     });
-  }).finally((): void => pm2.disconnect());
+  });
 };
 
 /**
- * Adds a message handler.
+ * Attaches a message handler for the given topic.
  */
-export const onMessage = function onMessage(topic: string, handler: (packet: Packet) => void): void {
+export const onMessage = function onMessage(topic: string, handler: MessageHandler): void {
   handlers[topic] = handler;
 };
 
